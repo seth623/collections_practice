7db27addc1652894e0e904caa8df67cb1a7600d4
seth <seth623@gmail.com>
2021-12-23 16:50:12 -0700

    Completed Assignment

--- collections_practice.rb
+++ collections_practice.rb
@@ -24,6 +24,51 @@ end
 
 def swap_elements(array)
 
-    array.sort { array[2] <=> array[1] }
+    array[1], array[2] = array[2], array[1]
+    array 
+
+end 
+
+def reverse_array(array)
+    
+    array.reverse 
+
+end
+
+
+def kesha_maker(array)
+
+    kesha_array = array.each { |word| word[2] = "$" }
+    return kesha_array
+
+end 
+
+
+def find_a(array)
+
+    array.select { |word| word.start_with?("a") }
+
+end 
+
+def sum_array(array)
+
+    array.inject(0) { |sum, number| sum += number}
+
+end 
+
+
+def add_s(array)
+
+    add_s_array = []
+    
+    array.each_with_index do |word, index|
+        if index != 1
+            add_s_array << (word << "s")
+        else 
+            add_s_array << word 
+        end 
+    end 
+
+    return add_s_array
 
 end 
