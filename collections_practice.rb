fc4faaca2846e8790b8879797abf3e4f5ccfd9de
seth <seth623@gmail.com>
2021-12-23 15:18:09 -0700

    current assignment

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
+def sort_array_asc(integer_array)
+    
+    integer_array.sort
 
+end
+
+
+def sort_array_desc(integer_array)
+    
+    integer_array.sort do |a, b|
+        b <=> a  
+    end
+
+end
+
+
+def sort_array_char_count(array)
+    
+    array.sort do |a, b|
+        a.length <=> b.length  
+    end
+
+end 
+
+def swap_elements(array)
+
+    array.sort { array[2] <=> array[1] }
+
+end 
